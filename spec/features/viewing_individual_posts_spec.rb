03531da3cf1b7b3abcc89d1768c367d22e5a12e3
cazyw <kazeisc@gmail.com>
2017-09-17 23:36:12 +1000

    Update stylesheet for post index page

--- spec/features/viewing_individual_posts_spec.rb
+++ spec/features/viewing_individual_posts_spec.rb
@@ -5,6 +5,6 @@ feature 'Can view individual posts' do
         post = create(:post)
         visit '/'
         find(:xpath, "//a[contains(@href,'posts/1')]").click
-        expect(page.current_page).to eq(post_path(post))
+        expect(page.current_path).to eq(post_path(post))
     end
 end
