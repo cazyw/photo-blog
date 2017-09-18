57422d50e2560faa7d9334bbcc0e3e9134a6d918
cazyw <kazeisc@gmail.com>
2017-09-18 22:08:39 +1000

    Updated the formatting to display a larger image and add a footer

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
     validates :caption, presence: { message: 'Warning: caption cannot be blank!' }
     validates :location, presence: { message: 'Warning: location cannot be blank!' }
     
-    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :image, styles: { large: "600x600#", medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
