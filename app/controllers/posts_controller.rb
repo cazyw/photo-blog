5d81beee385383e5982e08aea4d1de295ccf4d4b
cazyw <kazeisc@gmail.com>
2017-09-20 22:17:34 +1000

    Restrict editing of posts to users logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :edit, :update,   :destroy]
+    before_action :authenticate_user!, :except => [:show, :index]
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+    
 
     def index
         @posts = Post.all
