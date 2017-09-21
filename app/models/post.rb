d017b119c57ae78e5ac63ef2c9a2f206e4a7a8b9
cazyw <kazeisc@gmail.com>
2017-09-22 03:27:04 +1000

    Add seed data for Heroku

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
     belongs_to :user
     validates :user_id, presence: true
-    validates :image, presence: { message: 'Warning: no image was added!' }
     validates :caption, presence: { message: 'Warning: caption cannot be blank!' }
     validates :location, presence: { message: 'Warning: location cannot be blank!' }
+    validates :image, presence: { message: 'Warning: no image was added!' }
     
     has_attached_file :image, styles: { large: "600x600#", medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
