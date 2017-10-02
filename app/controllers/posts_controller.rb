e4b0aa0cdda251d5b705de218336ba5cda5e4eba
cazyw <kazeisc@gmail.com>
2017-10-02 20:48:15 +1100

    Change order of posts to use created time

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
     
 
     def index
-        @posts = Post.order("updated_at DESC").all
+        @posts = Post.order("created_at DESC").all
     end
 
     def show
